f514c821a8453fecdbaad7e62f52626309617283
Stalin-Cordoba <wisquiquikiri13@gmail.com>
2025-12-01 20:04:04 -0600

    A la hora de ingresar las ecuaciones, ahora si permite como superíndice la letra 'x'

--- models/interfaz.py
+++ models/interfaz.py
@@ -224,7 +224,7 @@ class Interfaz:
 
             w.destroy()
         
-        self.superindices = {"0": "⁰","1": "¹","2": "²","3": "³","4": "⁴","5": "⁵","6": "⁶","7": "⁷","8": "⁸","9": "⁹"}
+        self.superindices = {"0": "⁰","1": "¹","2": "²","3": "³","4": "⁴","5": "⁵","6": "⁶","7": "⁷","8": "⁸","9": "⁹", "x":"ˣ"}
 
         def procesar(evento):
             texto = self.ecuacion.get()
@@ -324,7 +324,7 @@ class Interfaz:
 
             w.destroy()
 
-            self.superindices = {"0": "⁰","1": "¹","2": "²","3": "³","4": "⁴","5": "⁵","6": "⁶","7": "⁷","8": "⁸","9": "⁹"}
+            self.superindices = {"0": "⁰","1": "¹","2": "²","3": "³","4": "⁴","5": "⁵","6": "⁶","7": "⁷","8": "⁸","9": "⁹", "x":"ˣ"}
 
         def procesar(evento):
             texto = self.ecuacion.get()
